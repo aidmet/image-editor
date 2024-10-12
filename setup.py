11522721bb4f39c9c3cadd29cb519f605db33685
aidmet <avaartshop@outlook.com>
2024-10-12 16:43:27 +0100

    Initial commit of image-editor library

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(
     name='image-editor',
     version='0.1',
     description='A simple image editing library with drawing tools and effects',
-    author='Your Name',
+    author='Aiden Metcalfe',
     packages=find_packages(),
     install_requires=[
         'Pillow',
     ],
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown"
 )
